253f10c8d646035267a1b8d9c605e56425847e6a
dgolov <dmitriy.evgenyevich@gmail.com>
2022-04-03 21:37:03 +0300

    updated routing

--- web/urls.py
+++ web/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 
-from .views import index
+from .views import MainView, AboutView, BoatView, ServiceView, GalleryView, ContactView
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', MainView.as_view(), name='main'),
+    path('about', AboutView.as_view(), name='about'),
+    path('boat', BoatView.as_view(), name='boat'),
+    path('service', ServiceView.as_view(), name='service'),
+    path('gallery', GalleryView.as_view(), name='gallery'),
+    path('contact', ContactView.as_view(), name='contact'),
 ]
--- web/views.py
+++ web/views.py
@@ -1,5 +1,50 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from django.views import View
 
 
-def index(request):
-    return render(request, 'index.html', {})
+class MainView(View):
+    """ Представление главной страницы
+    """
+    def get(self, request, *args, **kwargs):
+        context = {}
+        return render(request, 'index.html', context)
+
+
+class AboutView(View):
+    """ Представление страницы обо мне
+    """
+    def get(self, request, *args, **kwargs):
+        context = {}
+        return render(request, 'about.html', context)
+
+
+class BoatView(View):
+    """ Представление страницы о лодке
+    """
+    def get(self, request, *args, **kwargs):
+        context = {}
+        return render(request, 'boat.html', context)
+
+
+class ContactView(View):
+    """ Представление страницы контакты
+    """
+    def get(self, request, *args, **kwargs):
+        context = {}
+        return render(request, 'contact.html', context)
+
+
+class ServiceView(View):
+    """ Представление страницы услуги
+    """
+    def get(self, request, *args, **kwargs):
+        context = {}
+        return render(request, 'service.html', context)
+
+
+class GalleryView(View):
+    """ Представление галерея
+    """
+    def get(self, request, *args, **kwargs):
+        context = {}
+        return render(request, 'gallery.html', context)
