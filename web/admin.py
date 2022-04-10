8226f0cf38ccdc0226117f3a954283a8fb2bc051
dgolov <dmitriy.evgenyevich@gmail.com>
2022-04-10 13:33:11 +0300

    updated admin.py

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from .models import Service, Photo, Review, Request
 
-# Register your models here.
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    """ Отображение услуг в админке
+    """
+    list_display = ['id', 'name']
+    list_display_links = ['id', 'name']
+    search_fields = ['name']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    """ Отображение услуг в админке
+    """
+    list_display = ['id', 'path']
+    list_display_links = ['id', 'path']
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    """ Отображение услуг в админке
+    """
+    list_display = ['id', 'name', 'created_at']
+    list_display_links = ['id', 'name']
+    search_fields = ['name']
+
+
+@admin.register(Request)
+class RequestAdmin(admin.ModelAdmin):
+    """ Отображение услуг в админке
+    """
+    list_display = ['id', 'name', 'created_at']
+    list_display_links = ['id', 'name']
+    search_fields = ['name']
