ae3dcfc69d6de6ca24147bb2020aeeeb62cbc1a0
dgolov <dmitriy.evgenyevich@gmail.com>
2022-05-02 12:52:29 +0300

    Added db settings

--- FominFishing/settings.py
+++ FominFishing/settings.py
@@ -62,8 +62,12 @@ WSGI_APPLICATION = 'FominFishing.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ.get('DB_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': os.environ.get('DB_DATABASE', os.path.join(BASE_DIR, 'db.sqlite3')),
+        'USER': os.environ.get('DB_USER', 'user'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'password'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
